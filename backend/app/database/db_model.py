f5e02c817c4c146ba9bb44a9ea0496f8744a7885
Walu064 <waluwalu064@gmail.com>
2023-12-25 10:36:49 +0100

    Aktualizacja bazy danych - nowe tabele: Teacher, Course, DeanGroup

--- backend/app/database/db_model.py
+++ backend/app/database/db_model.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -15,12 +15,32 @@ class User(Base):
     album_number = Column(String, unique=True, index=True)
     hashed_password = Column(String)
 
+class Teacher(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    courses = relationship("Course", backref="teacher")
+
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+    dean_group_id = Column(Integer, ForeignKey('dean_groups.id'))
+
+class DeanGroup(Base):
+    __tablename__ = "dean_groups"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    students = relationship("Student", backref="dean_group")
+    schedule = relationship("Course", backref="dean_group")
+
 class Student(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
     album_number = Column(String, unique=True, index=True)
-    dean_group = Column(String, index=True)
+    dean_group_id = Column(Integer, ForeignKey('dean_groups.id'))
 
 Base.metadata.create_all(bind=engine)
